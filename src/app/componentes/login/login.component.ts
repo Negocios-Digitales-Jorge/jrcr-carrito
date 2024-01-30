1fb2f5c3f1b4fb2afc51354ddc313bcd1898f96f
Jorge Raúl Cruz Reyes <colegioagazzijorge1sec@gmail.com>
2024-01-30 11:46:07 -0600

    Version 1.4

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+// Definir la expresión regular aquí
+const strongPasswordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$/;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginForma = this.fb.group({
     email: ['', Validators.required],
-    password: ['', Validators.required]
+    password: ['', [Validators.required, Validators.pattern(strongPasswordPattern)]]
   });
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {}
 
+  get email() {
+    return this.loginForma.controls['email'];
+  }
+
+  get password() {
+    return this.loginForma.controls['password'];
   }
 }
