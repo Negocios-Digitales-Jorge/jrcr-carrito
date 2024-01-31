b85761c6eed855fd8de9956fa592c934e2b3eec9
Jorge Raúl Cruz Reyes <colegioagazzijorge1sec@gmail.com>
2024-01-31 13:53:19 -0600

    Version 1.5

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../../shared/password-match.directives';
+
+const strongPasswordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?\/\\\-]).{8,}$/;
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+[x: string]: any;
   registerForma = this.fb.group({
-    "name":['',[Validators.required]]
-  })
-  constructor(private fb: FormBuilder) { }
+    fullName: ['', [Validators.required,Validators.pattern(/^[a-zA-Z]+(?:[a-zA-Z]+)*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required,Validators.pattern(strongPasswordPattern)]],
+    confirmPassword: ['', [Validators.required]],
+  },{
+    validators:passwordMatchValidator
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get fullName() {
+    return this.registerForma.controls['fullName'];
+  }
+
+  get email() {
+    return this.registerForma.controls['email'];
+  }
+
+  get password() {
+    return this.registerForma.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForma.controls['confirmPassword'];
+  }
 }
